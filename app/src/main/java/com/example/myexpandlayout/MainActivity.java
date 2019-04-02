1fc625d5af3de3fb6d135924e35d4507177aca86
technologist25 <priteshvishwakarma@technologist.com>
2019-04-02 10:52:42 +0530

    new view animation added

--- app/src/main/java/com/example/myexpandlayout/MainActivity.java
+++ app/src/main/java/com/example/myexpandlayout/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.myexpandlayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.myexpandlayout.expand.ExpandableLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private ExpandableLayout expandableLayout1;
+    ImageView iv;
 
 
     @Override
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         expandableLayout1 = findViewById(R.id.expandable_layout_1);
+        iv = findViewById(R.id.iv);
 
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
@@ -24,8 +28,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (expandableLayout1.isExpanded()) {
                     expandableLayout1.collapse();
+                    iv.animate().rotation(180).start();//startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.clockwise));
                 } else {
                     expandableLayout1.expand();
+                    iv.animate().rotation(0).start();//startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.anti_clockwise));
                 }
             }
         });
